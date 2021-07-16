4e4b07e60eebe439de516a8cdbf2338f6a51dbe8
Santino Valenzuela <s0nnyv123@gmail.com>
2021-07-16 15:38:36 -0400

    Setting up index php file by adding description about the application and technologies used

--- index.php
+++ index.php
@@ -8,10 +8,30 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css" integrity="sha512-OdEXQYCOldjqUEsuMKsZRj93Ht23QRlhIb8E/X0sbwZhme8eUw6g8q7AdxGJKakcBbv7+/PX0Gc2btf7Ru8cZA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/solid.min.css" integrity="sha512-jQqzj2vHVxA/yCojT8pVZjKGOe9UmoYvnOuM/2sQ110vxiajBU+4WkyRs1ODMmd4AfntwUEV4J+VfM6DkfjLRg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="css/style.css" />
-  <title>Document</title>
+  <title>Movieflix CRUD</title>
 </head>
 <body>
-  
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>Movieflix CRUD</h1>
+        <p>Technologies used:<br />
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>PHP</li>
+            <li>MySQL</li>
+          </ul>
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        
+      </div>
+    </div>
+  </div>
   <script type="text/javascript" href="js/script.js"></script>
 </body>
 </html>
